810918f6447f5688b9c41f9b2aac3bc877e7a541
Irman Mashuri <tech.platform@aptana.co.id>
2026-02-02 16:19:42 +0800

    feat: connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
 #include <Arduino.h>
 #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+const char *ssid = "Unlisted-MobileHotspot";
+const char *password = "anuku123456";
+
+void connectToWiFi()
+{
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // Indicate connection attempt
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making
-  delay(1000);                       // wait for a second
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  connectToWiFi();
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making
+  delay(1000);                     // wait for a second
 }
