cfde7eec580dfdaa50611f07325e12335bac1904
Irman Mashuri <tech.platform@aptana.co.id>
2026-02-03 12:23:20 +0800

    feat: connect to mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,88 @@
-#include <Arduino.h>
-#include <WiFiClient.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-const char *ssid = "Unlisted-MobileHotspot";
-const char *password = "anuku123456";
+// WiFi settings
+const char *ssid = "Unlisted-MobileHotspot";         // Replace with your WiFi name
+const char *password = "anuku123456"; // Replace with your WiFi password
 
-void connectToWiFi()
-{
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+// MQTT Broker settings
+const char *mqtt_broker = "178.128.125.24"; // EMQX broker endpoint
+const char *mqtt_topic = "/";    // MQTT topic
+// const char *mqtt_username = "emqx";         // MQTT username for authentication
+// const char *mqtt_password = "public";       // MQTT password for authentication
+const int mqtt_port = 1883;                 // MQTT port (TCP)
 
-  WiFi.begin(ssid, password);
+WiFiClient espClient;
+PubSubClient mqtt_client(espClient);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    digitalWrite(LED_BUILTIN, HIGH); // Indicate connection attempt
-    delay(500);
-    Serial.print(".");
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+void connectToWiFi();
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
+void connectToMQTTBroker();
+
+void mqttCallback(char *topic, byte *payload, unsigned int length);
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-
   connectToWiFi();
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+  mqtt_client.setCallback(mqttCallback);
+  connectToMQTTBroker();
+}
+
+void connectToWiFi()
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to the WiFi network");
+}
+
+void connectToMQTTBroker()
+{
+  while (!mqtt_client.connected())
+  {
+    String client_id = "esp8266-client-" + String(WiFi.macAddress());
+    Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
+    if (mqtt_client.connect(client_id.c_str()))
+    {
+      Serial.println("Connected to MQTT broker");
+      mqtt_client.subscribe(mqtt_topic);
+      // Publish message upon successful connection
+      mqtt_client.publish(mqtt_topic, "Hi I'm ESP8266 ^^");
+    }
+    else
+    {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message received on topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making
-  delay(1000);                     // wait for a second
+  if (!mqtt_client.connected())
+  {
+    connectToMQTTBroker();
+  }
+  mqtt_client.loop();
 }
